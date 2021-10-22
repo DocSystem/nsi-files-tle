a40b998543e8e0a6baedd372cd9b4dea51db5f7b
DocSystem <42613513+DocSystem@users.noreply.github.com>
2021-10-22 11:49:33 +0200

    Removed some useless things

--- Entrainement Epreuve 28/ex2.py
+++ Entrainement Epreuve 28/ex2.py
@@ -7,17 +7,3 @@ def tri_iteratif(tab):
         if tab[imax] > tab[k-1] :
             tab[k-1], tab[imax] = tab[imax], tab[k-1]
     return tab
-
-"""
-def tri_iteratif_1(tab):
-    M=tab[0]
-    iM = 0
-    for i in range(len(tab)):
-        if tab[i]>M:
-            M=tab[i]
-            iM=i
-    if M>tab[-1]:
-        tab[iM] = tab[-1]
-        tab[-1] = M
-    return tab
-"""
